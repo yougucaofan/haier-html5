db7588be9c3d00fc278609bc06ded0891a35bb3c
smallgrass <yougucaofan@126.com>
2014-08-10 19:36:16 +0800

    over

--- html/js/style.js
+++ html/js/style.js
@@ -1,31 +1,60 @@
 function pageEffect(obj) {
 	this.container = $('#container');
 	this.wrap = this.container.find('.wrap');
-	this.clipElem = this.wrap.add(this.container);
+	this.clipElem = this.wrap.add('.body-wrap');
+
+	// 第三屏隐藏图片
+	this.entity = $('#entity');
 	this.count = 0;
+	this.state = true;
 	this.max = this.wrap.length - 1;
 
 	// 初始化
 	this.init();
 }
 pageEffect.prototype = {
-	init:function() {
-		var self = this;
-		
-		// 是否支持css3
-		this.isSupCss3();		
 
-		// 绑定resize实时监控高度/宽度
-		$(window).bind('resize', function() {
-			self.monitoringBody.call(self);
-		}).resize();
+	// 储存各各的运行时间
+	transitTime: [300, 300 ,1200 ,800 ,800 ,800 ,800 ,800 , 0, 800, 800],
+	init:function() {
+
+		// 是否支持css3
+		this.isSupCss3();
+
+		// 添加右侧控制圆点
+		this.addHandle();		
 
 		// 第一屏slider效果
 		this.slider();
 
-		// 绑定鼠标中键事件
-		this.addWheel();
+		// 添加各类事件
+		this.addEvent();
+
+		// 初始化选中元素
+		this.handle.first().addClass('on');
 	},
+
+	// 右边控制手柄
+	addHandle:function() {
+		var len = this.wrap.length,
+			i = 0,
+			str = '',
+			w;
+		for(; i < len ; i++) {
+			str += '<span></span>';
+		};
+		str = '<p id="pager">' + str + '</p>';
+
+		// 保存控制器
+		this.handleWrap = $(str).insertAfter(this.container);
+		this.handle = this.handleWrap.find('span');
+
+		// 垂直居中
+		h = this.handleWrap.height();
+		this.handleWrap.css('margin-top', -h/2);
+	},
+
+	// 第一屏slider
 	slider:function() {
 		var wrap = $('#slider'),
 			mover = wrap.find('ul'),
@@ -39,7 +68,8 @@ pageEffect.prototype = {
 			index = 0,
 			startGo = $('#startGo'),
 			changePic = $('#jumpPic'),
-			address = ['pic2.jpg', 'pic3.jpg', 'pic4.jpg'],
+			address = ['', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg'],
+			self = this,
 			current, setInterval_elem;
 
 		// 初始化
@@ -62,42 +92,44 @@ pageEffect.prototype = {
 				}
 			};
 			_move();
-		})
+		});
 
 		// 移动函数
 		function _move() {
 			index++;
 			current = -index*w + maxW;
-			mover.animate({marginLeft: current}, function() {
-
-			})
+			mover.animate({marginLeft: current});
 		};
 
 		// 开始烘焙跳转
 		startGo.click(function() {
 			changePic.attr('src', 'images/' + address[index]);
+			self.count++;
+			self.setContainerVal();
 			return false;
 		})
 	},
-	suppFun:function() {
-		// 获取浏览器前缀
-		this.getPrefix();
-		this.container.addClass('cs3-transition');
+
+	// 添加各种event
+	addEvent:function() {
+		var self = this;
+		// 绑定resize实时监控高度/宽度
+		$(window).bind('resize', function() {
+			self.winResizeFun.call(self);
+		}).resize();
+
+		// 添加鼠标中键控制
+		this.addWheel();
+
+		// 右侧小点添加事件
+		this.handle.bind('click', function() {
+			if(!self.state) return;
+			self.count = $(this).index();
+			self.setContainerVal();
+		})
 	},
-	moveFun:function() {
-		//
-	},
-	getPrefix: function() {
-		var ua = navigator.userAgent,
-			browserType = $.uaMatch(ua).browser,
-			obj = {
-				'webkit': '-webkit-',
-				'mozilla': '-moz-',
-				'opera': '-o-',
-				'msie': '-ms-',
-			};
-		this.prefix = obj[browserType];
-	},
+
+	// 添加鼠标中键
 	addWheel:function() {
 		var body = document.body,
 			self = this;
@@ -113,6 +145,10 @@ pageEffect.prototype = {
 			};
 		};
 		function _scroll(e) {
+			// 如果container正在动画 或者 本pagecss动画没有完成则return
+			if(this.container.is(':animated') || !this.state) {
+				 return false
+			};
 			var val;
 
 			// opera两个都支持所以要把wheelDelta放前面，孤立ff
@@ -122,18 +158,19 @@ pageEffect.prototype = {
 			} else {
 				this.count--;
 			};
-			this.wheelFun();
+			if(this.count < 0) {
+				this.count = 0;
+			};
+			if(this.count > this.max){
+				this.count = this.max;
+			};
+
+			// 设置container的位置
+			this.setContainerVal();
 		}
 	},
-	wheelFun:function() {
-		if(this.count < 0) {
-			this.count = 0;
-		};
-		if(this.count > this.max){
-			this.count = this.max;
-		};
-		this.setContainerVal();
-	},
+
+	// 是否支持css动画效果，通过属性判断区分浏览器
 	isSupCss3: function() {
 	    var div = document.createElement('div'),
 	        transEndEventNames = {
@@ -151,26 +188,67 @@ pageEffect.prototype = {
 	    };
 
 	    this.isSup = !!type;
-	    if(this.isSup) {
-			this.suppFun();
+
+	    // 如果不支持就默认归为
+	    if(!this.isSup) {
+			this.wrap.addClass('on');
 		} else {
-			this.moveFun();
+			this.wrap.first().addClass('on');
 		};
 	},
-	monitoringBody:function() {
+
+	// 监视窗口大小变化
+	winResizeFun:function() {
 		this.w = $(window).width();
 		this.h = $(window).height();
 		this.clipElem.css('height', this.h);
-		this.setContainerVal();
+		this.setContainerVal(true);
 	},
-	setContainerVal:function(v) {
-		var val = - (v || this.count) * (this.h),
-			prefix = this.prefix;
-		if(this.isSupCss3) {
-			this.container.css(prefix + 'transform', 'translate3d(0, '+ val + 'px, 0)');
+
+	// 设置container的位置
+	setContainerVal:function(state) {
+		var val = - (this.count) * (this.h),
+			self = this,
+			isSup = this.isSup,
+			time = isSup ? 600 : 0,
+			count = this.count;
+
+		// 这个用于窗口大小变化时改变container的top坐标，没动画
+		if(state) {
+			this.container.css('top', val);
+
+		// 动画切换container位置
 		} else {
-			this.container.stop().animate({marginTop: val});
-		};
+			if(!this.state) return;
+
+			// 支持css3动画的才设置
+			if(isSup) {
+				this.state = false;
+			}
+
+			this.container.animate({'top': val}, 500 , function() {
+				self.handle.removeClass('on').eq(count).addClass('on');
+
+				// 对于高端浏览器才切换on状态。低端则一直都存在on
+				if(isSup) {
+					self.wrap.removeClass('on').eq(count).addClass('on');
+
+					// 注册时间，等动画完了才能下一次动画
+					window.setTimeout(function() {
+						self.state = true;
+					}, self.transitTime[count]);
+				};
+
+				// 这时为第三屏幕。动画完后要动画完成第二张图的显示
+				if(count==2) {
+					window.setTimeout(function(){
+						self.entity.animate({'opacity': 1}, 600);
+					}, time);
+				} else {
+					self.entity.stop().removeAttr('style');
+				};
+			});			
+		}
 	}
 };
 
